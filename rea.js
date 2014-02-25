5171c7e98e7e13ddca21d14edeeb94d371bc7ec6
Grant Miiller <grant@m2iller.com>
2014-02-25 12:18:17 -0500

    Initial commit

---
+++ rea.js
@@ -0,0 +1,109 @@
+/*
+*   Rea.Js (Reagis, get it? Ha Ha Ha)
+*   @author     Grant Miiller
+*   @version    0.0.1
+*   @classDesc  Reactive JS
+*
+*   USAGE
+*   ----------------------------------
+*   Used to call JS at certain break points
+*
+*/
+
+/*
+*   RELEASE NOTES
+*   
+*   Version 0.1.1 - Grant Miiller
+*   -Initial setup of everything
+*/
+
+
+(function(undefined) {
+  // Check for TNY global namespace
+  if(!window.TNY) { window.TNY  =   {}; }
+  var TNY = window.TNY;
+
+  TNY.reajs = (function () {
+   
+    var breakpoints = {},
+        noop        = function(){},
+        console     = window.console || {"log": noop, "warn": noop, "error": noop},
+        viewportSize  = window.innerWidth;
+   
+    function checkViewPort(key) {
+      if(breakpoints[key].min < viewportSize) {
+
+        if(breakpoints[key].max) {
+          if( breakpoints[key].max > viewportSize ) {
+            return true;
+          }
+        } else {
+          return true;
+        }
+      }
+
+      if(breakpoints[key].max && !breakpoints[key].min && breakpoints[key].max > viewportSize) {
+        return true;
+      }
+
+      return false;
+    }
+
+    // window.addEventListener('resize', function() {
+    //   viewportSize = window.innerWidth;
+
+    //   for(var key in breakpoints) {
+    //     if(checkViewPort(key)) {
+    //       breakpoints[key].callbacks[0]();
+    //     }
+    //   }
+    // }, false);
+
+    return {
+   
+      addBreakpoint: function (key, opts) {
+        if(!key) {
+          console.log('Please specify a label');
+          return;
+        }
+
+        if(!opts || (!opts.min && !opts.max)) {
+          console.log('Must provide minimum or maximum breakpoint');
+          return;
+        }
+
+        breakpoints[key] = {
+          min:        opts.min || null,
+          max:        opts.max || null,
+          callbacks : []
+        };
+
+        return this;
+      },
+
+      fire: function(key, callback, context) {
+        var args = Array.prototype.slice.call(arguments, 3);
+
+        if(!key || !callback) {
+          console.log( 'Please provide needed parameters' );
+          return false;
+        }
+
+        context = context || null;
+
+        if(checkViewPort(key)) {
+          callback.apply(context, args);
+        }
+      },
+
+      checkViewPort: checkViewPort
+    }
+
+
+  })();
+
+})();
+
+TNY.reajs.addBreakpoint('desktop', {min: 851});
+TNY.reajs.addBreakpoint('tablet', {min: 601, max: 850});
+TNY.reajs.addBreakpoint('mobile', {max: 600});
