8f98541239a98696837ac74ca90d98b4e9d2ca22
Grant Miiller <grant@m2iller.com>
2014-02-25 15:04:49 -0500

    Added event listener to fire when entering breakpoint

--- rea.js
+++ rea.js
@@ -19,12 +19,49 @@
 
 
 (function(undefined) {
+
+  //START Polyfills
+
+  if (!Array.prototype.indexOf) {
+    Array.prototype.indexOf = function (searchElement, fromIndex) {
+      if ( this === undefined || this === null ) {
+        throw new TypeError( '"this" is null or not defined' );
+      }
+
+      var length = this.length >>> 0; // Hack to convert object.length to a UInt32
+
+      fromIndex = +fromIndex || 0;
+
+      if (Math.abs(fromIndex) === Infinity) {
+        fromIndex = 0;
+      }
+
+      if (fromIndex < 0) {
+        fromIndex += length;
+        if (fromIndex < 0) {
+          fromIndex = 0;
+        }
+      }
+
+      for (;fromIndex < length; fromIndex++) {
+        if (this[fromIndex] === searchElement) {
+          return fromIndex;
+        }
+      }
+
+      return -1;
+    };
+  }
+
+  // END Polyfills
+
   var Reajs = (function () {
    
-    var breakpoints = {},
-        noop        = function(){},
-        console     = window.console || {"log": noop, "warn": noop, "error": noop},
-        viewportSize  = window.innerWidth;
+    var breakpoints         = {},
+        activeBreakpoints   = [],
+        noop                = function(){},
+        console             = window.console || {"log": noop, "warn": noop, "error": noop},
+        viewportSize        = window.innerWidth;
    
     function checkViewPort(key) {
       if(breakpoints[key].min < viewportSize) {
@@ -45,15 +82,56 @@
       return false;
     }
 
-    // window.addEventListener('resize', function() {
-    //   viewportSize = window.innerWidth;
+    function addActiveBreakpoint(key) {
+      if(activeBreakpoints.indexOf(key) === -1 ) {
+        activeBreakpoints.push(key);
+      }
 
-    //   for(var key in breakpoints) {
-    //     if(checkViewPort(key)) {
-    //       breakpoints[key].callbacks[0]();
-    //     }
-    //   }
-    // }, false);
+      return this;
+    }
+
+    function removeActiveBreakpoint(key) {
+      var i = activeBreakpoints.indexOf(key);
+
+      if(i !== -1) {
+        activeBreakpoints.splice(i, 1);
+      }
+    }
+
+    function checkActiveBreakpoint(key) {
+      console.log(activeBreakpoints.indexOf(key));
+      if(activeBreakpoints.indexOf(key) === -1 ) {
+        return false;
+      }
+      return true;
+    }
+
+    function fireCallbacks(key) {
+      var callbacks = breakpoints[key].callbacks,
+          callback = null;
+
+      for(var i = 0, len = callbacks.length; i < len; i++ ) {
+        callback = callbacks[i];
+        callback.func.apply(callback.context, callback.args);
+      }
+    }
+
+    /* Watch to see when to fire events */
+
+    window.addEventListener('resize', function() {
+      viewportSize = window.innerWidth;
+
+      for(var key in breakpoints) {
+        if(checkViewPort(key)) {
+          if(!checkActiveBreakpoint(key)) {
+            addActiveBreakpoint(key);
+            fireCallbacks(key);
+          }
+        } else {
+          removeActiveBreakpoint(key);
+        }
+      }
+    }, false);
 
     return {
    
@@ -74,14 +152,49 @@
           callbacks : []
         };
 
+        checkViewPort(key);
+
         return this;
       },
+
+      registerCallback: function(key, flags, callback, context) {
+
+      },
+
+      onBreakpoint: function(key, callback, context) {
+        var args = Array.prototype.slice.call(arguments, 3);
+
+        if(!key || !callback) {
+          console.log( 'Please provide needed paramters' );
+          return false;
+        }
+
+        if(!breakpoints[key]) {
+          console.log('Breakpoint does not exist');
+          return false;
+        }
+
+        context = context || null;
+
+        breakpoints[key].callbacks.push({
+          func: callback,
+          context: context, 
+          args: args
+        }); 
+      },
+
+      fireCallbacks: fireCallbacks,
 
       fire: function(key, callback, context) {
         var args = Array.prototype.slice.call(arguments, 3);
 
         if(!key || !callback) {
           console.log( 'Please provide needed parameters' );
+          return false;
+        }
+
+        if(!breakpoints[key]) {
+          console.log('Breakpoint does not exist');
           return false;
         }
 
@@ -97,7 +210,7 @@
 
 
   })();
-  
+
   window.Reajs = Reajs;
 
 })();
